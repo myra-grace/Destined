c18705f7a30419318bc55dbfbd77f1b54f63e768
Myra S <59889116+myra-grace@users.noreply.github.com>
2023-09-17 23:51:35 -0400

    remove react strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
